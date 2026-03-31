45975023fcf7970c2b8cc7c90a840f26671ccf63
admin <emlqld@naver.com>
2026-03-31 10:31:25 +0900

    A형기출-보급로-PASS

--- Algorithm/src/SAMSUNG_A/D4_1249_보급로.java
+++ Algorithm/src/SAMSUNG_A/D4_1249_보급로.java
@@ -13,7 +13,7 @@ public class D4_1249_보급로 {
 		for(int tc = 1; tc < T+1;tc++) {
 			N = Integer.parseInt(br.readLine());
 			board = new char[N][N];
-			visited = new boolean[N][N];
+			
 			for(int i = 0; i < N; i++) {
 				String str = br.readLine();
 				board[i] = str.toCharArray();
@@ -22,34 +22,54 @@ public class D4_1249_보급로 {
 			System.out.println("#"+tc+" "+bfs());
 		}
 	}
-	public static boolean[][] visited;
+
 	static final int[] dx = {0,0,1,-1};
 	static final int[] dy = {1,-1,0,0};
+	static final int INF = Integer.MAX_VALUE / 2;
+	
+	public static class Node{
+		int x, y, time;
+		boolean[][] visited;
+		public Node(int x, int y, int time) {
+			this.x = x;
+			this.y = y;
+			this.time = time;
+		}
+	}
 	public static int bfs() {
-		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[] {0, 0, 0});
-		visited[0][0] = true;
-		int minTime = Integer.MAX_VALUE;
-		while(!q.isEmpty()) {
-			int[] cur = q.poll();
-			int x = cur[0], y = cur[1];
-			int time = cur[2];
+		int[][] dist = new int[N][N];
+		for(int[] arr : dist) Arrays.fill(arr, INF);
+		
+		PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.time - b.time);
+		pq.offer(new Node(0,0,0));
+		dist[0][0] = 0;
+		
+		
+		while(!pq.isEmpty()) {
+			Node n = pq.poll();
+			int x = n.x, y = n.y;
+			int time = n.time;
 			
-			if(x == N-1 && y ==N-1) {
-				minTime = Math.min(minTime, time);
-			}
+			if(time > dist[x][y]) continue;
 			
-			for(int t = 0; t < 4;t++) {
+			for(int t = 0; t < 4; t++) {
 				int nx = x + dx[t];
 				int ny = y + dy[t];
 				
 				if(nx >= N || ny >= N || nx < 0 || ny < 0) continue;
-				if(visited[nx][ny]) continue;
 				
-				q.offer(new int[] {nx, ny, time+(int)board[nx][ny]- '0'});
-				visited[nx][ny] = true;
+				int newTime = time + board[nx][ny]-'0';
+				
+				if(newTime < dist[nx][ny]) {
+					dist[nx][ny] = newTime;
+					pq.offer(new Node(nx, ny, newTime));
+				}
 			}
 		}
-		return minTime;
+		
+		
+		
+		
+		return dist[N-1][N-1];
 	}
 }
