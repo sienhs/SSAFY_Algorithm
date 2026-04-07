ce94a9c5e397fecafa29139c898c3405559e708a
admin <emlqld@naver.com>
2026-04-07 09:48:00 +0900

    simulation(MorningAlgo)*1

--- Algorithm/src/boj/시뮬/S5_3060.java
+++ Algorithm/src/boj/시뮬/S5_3060.java
@@ -1,5 +1,34 @@
 package boj.시뮬;
 
-public class S5_3060 {
+import java.util.*;
+import java.io.*;
 
+public class S5_3060 {
+	// (index-1)+(index+1)+((index+3)%6)
+	static int T;
+	static int[] pig;
+	static int totalCost;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1;tc < T+1;tc++) {
+			long food = Integer.parseInt(br.readLine());
+			totalCost = 0;
+			pig = new int[6];
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < 6; i++) {
+				pig[i] = Integer.parseInt(st.nextToken());
+			}
+			totalCost = Arrays.stream(pig).sum();
+			int days = 1;
+			while(totalCost <= food) {
+				totalCost*=4;
+				days++;
+			}
+			System.out.println(days);
+		}
+	}
+	
 }
